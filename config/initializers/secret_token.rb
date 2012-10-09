389a7479ac4e11a2910f1ba61bbbf773b7c0fa88
Adam Derewecki <adam@causes.com>
2012-10-09 07:51:13 -0700

    initial commit; 'rails new formr'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formr::Application.config.secret_token = 'afb15ca360cb0a2abfc2d5d83f95a00032b4f64662d511cfc4de7c8673e49d61d2dc3c747d987f103ed838aa9afb8b34c1c9d569b79e814520967c6397303a41'
