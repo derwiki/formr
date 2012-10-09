ffe4b04c263e15ae242ea7736b740e4a8e1788dc
Adam Derewecki <adam@causes.com>
2012-10-09 08:55:10 -0700

    MVP: JSON in, form out

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,103 @@
+class FormsController < ApplicationController
+  # GET /forms
+  # GET /forms.json
+  def index
+    @forms = Form.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @forms }
+    end
+  end
+
+  # GET /forms/1
+  # GET /forms/1.json
+  def show
+    @form = Form.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @form }
+    end
+  end
+
+  # GET /forms/new
+  # GET /forms/new.json
+  def new
+    @form = Form.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @form }
+    end
+  end
+
+  # GET /forms/1/edit
+  def edit
+    @form = Form.find(params[:id])
+  end
+
+  # POST /forms
+  # POST /forms.json
+  def create
+    if json_path = params[:file]
+      json = JSON.parse(json_path.read)
+      unless json.is_a? Array
+        raise StandardError, 'Input file needs to be JSON array'
+      end
+      json.each do |dict|
+        unless dict.is_a? Hash
+          raise StandardError, 'Input JSON needs to be a list of dictionaries'
+        end
+      end
+      json.each do |qa|
+        question = Question.create! text: qa['question']
+        %w(1 2 3 4).each do |nr|
+          Answer.create! text: qa[nr], question_id: question.id
+        end
+      end
+      #TODO if this crashes, things are in a bit of an inconsistent state
+      # there's probably a much better way to to nested model saving than this
+      render :json => json
+      return
+    end
+
+    respond_to do |format|
+      if @form.save
+        format.html { redirect_to @form, notice: 'Form was successfully created.' }
+        format.json { render json: @form, status: :created, location: @form }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @form.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /forms/1
+  # PUT /forms/1.json
+  def update
+    @form = Form.find(params[:id])
+
+    respond_to do |format|
+      if @form.update_attributes(params[:form])
+        format.html { redirect_to @form, notice: 'Form was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @form.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /forms/1
+  # DELETE /forms/1.json
+  def destroy
+    @form = Form.find(params[:id])
+    @form.destroy
+
+    respond_to do |format|
+      format.html { redirect_to forms_url }
+      format.json { head :no_content }
+    end
+  end
+end
